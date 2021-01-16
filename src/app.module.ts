0984e19443ad4846cd6d620b28d571fdd51a4ede
Alice Kile <buoyantair@protonmail.com>
2021-01-16 17:41:05 +0530

    books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { BooksController } from './books/books.controller';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController, BooksController],
+  imports: [BooksModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Book } from './interfaces/book.interface';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {
+
+  }
+  @Post()
+  async create(@Body() createBookDto: CreateBookDto) {
+    this.booksService.create(createBookDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Book[]> {
+    return this.booksService.findAll();
+  }
+}
